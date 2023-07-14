a2a9df12794525a93b661a51db1382269b759b2f
Isaque <zazac3179@gmail.com>
2023-07-14 15:29:03 -0300

    implementação da função de leitura de arquivos txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+        return []
+
+    try:
+        file = open(path_file, mode="r")
+        file_contents = file.read().split('\n')
+        file.close()
+        return file_contents
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return []

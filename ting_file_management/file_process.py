ad8ceab90ea4f15bd24a9276ef76f200f4b16a71
Isaque <zazac3179@gmail.com>
2023-07-14 16:02:30 -0300

    implementação da função process que estrutura o conteúdo da lista gerada pela função txt_importer

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def process(path_file, instance: Queue):
+    for i in range(len(instance._data)):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
+            return None
+
+    text_list = txt_importer(path_file)
+    dic = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(text_list),
+        "linhas_do_arquivo": text_list,
+    }
+    instance.enqueue(dic)
+    print(dic)
+    return dic
 
 
 def remove(instance):
